ca91a8c16bf611a76a45d4744ec42771625c7012
Samuel <samuel.rufinatscha@gmail.com>
2019-06-03 16:46:27 +0200

    Make sure that generated hashes have bundle head flag set

--- src/main/java/org/iota/ict/ixi/util/Generator.java
+++ src/main/java/org/iota/ict/ixi/util/Generator.java
@@ -1,5 +1,9 @@
 package org.iota.ict.ixi.util;
 
+import org.iota.ict.model.transaction.Transaction;
+import org.iota.ict.utils.Constants;
+import org.iota.ict.utils.Trytes;
+
 import java.security.SecureRandom;
 
 public class Generator {
@@ -12,6 +16,29 @@ public class Generator {
         for( int i = 0; i < 81; i++ )
             sb.append( alphabet.charAt( r.nextInt(alphabet.length()) ) );
         return sb.toString();
+    }
+
+    public static String getRandomBundleHead() {
+        String hash = getRandomHash();
+        while(!isBundleHead(hash))
+            hash = getRandomHash();
+        return hash;
+    }
+
+    public static boolean isBundleHead(String hash){
+        byte[] hashTrits = Trytes.toTrits(hash);
+        return isFlagSet(hashTrits, Constants.HashFlags.BUNDLE_HEAD_FLAG);
+    }
+
+    private static boolean isFlagSet(byte[] hashTrits, int position) {
+        assert hashTrits.length == Transaction.Field.TRUNK_HASH.tritLength;
+        return hashTrits[position] == 1;
+    }
+
+    public static void main(String[] args) {
+        String h = "TKTGCHDTRSC9GDYVXFTAFGGBTRVJHPSEZURRJ99XXJJ9ZGVEHSJABGEVYLWRRPCXAKF99BUORDXHRCZKQ";
+
+        System.out.println(isBundleHead(h));
     }
 
 }
--- src/test/java/SerializationTest.java
+++ src/test/java/SerializationTest.java
@@ -1,6 +1,8 @@
 import org.iota.ict.eee.call.FunctionEnvironment;
 import org.iota.ict.ixi.util.Generator;
 import org.iota.ict.ixi.util.TestTemplate;
+import org.iota.ict.utils.Trytes;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class SerializationTest extends TestTemplate {
@@ -10,30 +12,42 @@ public class SerializationTest extends TestTemplate {
 
         // publish a ClassFragment:
 
-        String requestId = "0";
-        String referenced_trunk_hash = Generator.getRandomHash();
-        String referenced_branch_hash = Generator.getRandomHash();
-        String ref0_classHash = "NULL";
-        String attribute0_size = "4";
-        String attribute1_size = "6";
+        String my_classname = "CLASS9FRAGMENT9TEST";
+        String referenced_trunk_hash = Generator.getRandomBundleHead();
+        String referenced_branch_hash = Generator.getRandomBundleHead();
+        String ref0_classHash = Trytes.NULL_HASH;
+        String attribute0_name = "FIRST9ATTRIBUTE";
+        String attribute0_size = "17";
+        String attribute1_name = "SECOND9ATTRIBUTE";
+        String attribute1_size = "17";
 
-        String args1 = requestId + ";" + referenced_trunk_hash + ";" + referenced_branch_hash + ";" + ref0_classHash + ";" + attribute0_size + ";" + attribute1_size;
-        String classHash = caller.call(new FunctionEnvironment("Serialization.ixi","publishClassFragment"), args1, 3000).split(";")[2];
+        String args1 =  my_classname + ";" +
+                        referenced_trunk_hash + ";" +
+                        referenced_branch_hash + ";" +
+                        ref0_classHash + ";" +
+                        attribute0_size + " " +
+                        attribute0_name + ";" +
+                        attribute1_size + " " +
+                        attribute1_name;
+
+        String classHash = caller.call(new FunctionEnvironment("Serialization.ixi", "publishClassFragment"), args1, 3000).split(";")[1];
+        Assert.assertNotNull(classHash);
 
         // publish a DataFragment:
 
-        String myOtherRequestId = "1";
-        String myOtherReferenced_trunk_hash = Generator.getRandomHash();
-        String myOtherReferenced_branch_hash = Generator.getRandomHash();
-        String data = "A 0 ATTRIB9ZERO9VALUE";
+        String myOtherReferenced_trunk_hash = Generator.getRandomBundleHead();
+        String myOtherReferenced_branch_hash = Generator.getRandomBundleHead();
+        String data = "A 0 ATTRIB9ZERO9VALUE;A 1 ANOTHER9ATTRIBUTE";
 
-        String args2 = myOtherRequestId + ";" + classHash + ";" + myOtherReferenced_trunk_hash + ";" + myOtherReferenced_branch_hash + ";" + data;
+        String args2 = classHash + ";" + myOtherReferenced_trunk_hash + ";" + myOtherReferenced_branch_hash + ";" + data;
 
-        FunctionEnvironment publishDataFragment = new FunctionEnvironment("Serialization.ixi","publishDataFragment");
-        ict1.submitEffect(publishDataFragment,args2);
+        String result = caller.call(new FunctionEnvironment("Serialization.ixi", "publishDataFragment"), args2, 3000);
+
+        Assert.assertTrue(result.length() > 0);
+
+        System.out.println(result);
 
     }
-
 
 
 }
